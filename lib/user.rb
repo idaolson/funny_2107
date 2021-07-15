db0fd870b4dd5c5d02bc6bfbe11548210df137cf
Ida Olson <idaolson@gmail.com>
2021-07-15 14:24:20 -0600

    Fix spacing in user class

--- lib/user.rb
+++ lib/user.rb
@@ -1,23 +1,22 @@
 class User
   attr_reader :name, :jokes
 
-  def initialize (name)
+  def initialize(name)
     @name = name
     @jokes = []
   end
 
-  def learn (joke)
+  def learn(joke)
     @jokes << joke
   end
 
-  def tell (user, joke)
+  def tell(user, joke)
     user.learn(joke)
   end
-  def joke_by_id (id)
+
+  def joke_by_id(id)
     @jokes.detect do |joke|
       joke.id == id
-  
     end
   end
-
 end
